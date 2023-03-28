fb314f1143ccf9eeb0ccc2ef5bf5abcbd7d3e017
cvinicius987 <caio.tecnologia@gmail.com>
2023-03-28 13:08:39 -0300

    Added java project

---
+++ java-prometheus-grafana/src/main/java/br/com/cvinicius/JavaPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cvinicius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPrometheusGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPrometheusGrafanaApplication.class, args);
+	}
+
+}
---
+++ java-prometheus-grafana/src/test/java/br/com/cvinicius/JavaPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cvinicius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPrometheusGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
